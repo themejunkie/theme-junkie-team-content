c26f827d59637d64e3f2ed917b4af2b01c2384cb
Satrya <satrya@satrya.me>
2014-04-25 21:16:06 +0700

    Remove add_image_size

--- inc/functions.php
+++ inc/functions.php
@@ -9,26 +9,8 @@
  * @license    http://www.gnu.org/licenses/gpl-2.0.html
  */
 
-/* Register new image size. */
-add_action( 'init', 'tjtc_add_image_size' );
-
 /* Remove uneeded theme-support. */
 add_action( 'init', 'tjtc_remove_theme_support_metabox', 11 );
-
-/**
- * Register new image size for post type edit screen.
- * 
- * @since  0.1.0
- * @access public
- */
-function tjtc_add_image_size() {
-
-	/* Make sure the theme support post thumbnail. */
-	add_theme_support( 'post-thumbnails' );
-
-	/* Register image size. */
-	add_image_size( 'tjtc-img', 70, 70, true );
-}
 
 /**
  * Remove uneeded theme-support for the post type.
